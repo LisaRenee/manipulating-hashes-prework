b80fbe0f6d02a8cf7b305d7c1f0e54d33957959e
Lisa Scott <lrscot@aol.com>
2019-02-12 04:41:32 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,8 +1,9 @@
+require "pry"
 def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -12,6 +13,7 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
+binding.pry
 
   #your code here
 
@@ -19,4 +21,3 @@ def first_challenge
   #remember to return your newly altered contacts hash!
   contacts
 end
-
